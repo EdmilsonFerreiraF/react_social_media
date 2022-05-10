a9a15d20e0582411f9ce474eb183621853111ff4
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-05-10 01:11:49 -0300

    🔨 :hammer: [Fix] Load sidebar friend picture

--- src/components/Sidebar/FriendItem/FriendItem.jsx
+++ src/components/Sidebar/FriendItem/FriendItem.jsx
@@ -4,14 +4,15 @@ import { useRequestImage } from 'hooks/useRequestImage'
 
 const FriendItem = ({ friend }) => {
     const profilePicture = useRequestImage("profile", friend?.profilePicture)
+    console.log('friend?.profilePicture - FriendItem', friend?.profilePicture)
 
     return (
         <li className={styles.friend}>
             <img className={styles.friendImg}
-            src={`img/${profilePicture}`}
+            src={profilePicture}
             alt="Friend profile" />
             <span className={styles.friendName}>
-                {friend.username}
+                {friend?.username}
             </span>
         </li>
     )
