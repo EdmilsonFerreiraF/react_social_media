5944e9f937713a2f2bf7882c6c5796e4351737f8
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-05-11 17:41:47 -0300

    🎨 :art: [Refactor] Split App.js for testing
    Separate ErrorBoundary and Router from App.js to index.js for testing as
    MemoryRouter is needed to prior testing

--- src/index.js
+++ src/index.js
@@ -2,11 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'App';
 import { AuthContextProvider } from "context/AuthContext"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+import ErrorFallback from 'components/ErrorFallback'
+
+import { ErrorBoundary } from 'react-error-boundary'
+
+import Error from 'pages/Error'
 
 ReactDOM.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onError={(error, errorInfo) => console.log({ error, errorInfo })}
+      >
+        <Router>
+          <App />
+        </Router>
+      </ErrorBoundary>
     </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
