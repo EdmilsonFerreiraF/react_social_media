b6edd074ce0873e208f7aceb8f38862cbbaed083
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-05-16 22:49:01 -0300

    🚀 :rocket: [Add] FollowingList - list role

--- src/components/MessagesBar/FollowingList/index.jsx
+++ src/components/MessagesBar/FollowingList/index.jsx
@@ -4,7 +4,7 @@ import styles from "./style.module.css"
 
 const FollowingList = ({ friends }) => {
     return (
-        <div className={styles.followingList}>
+        <div className={styles.followingList} role="list">
             {friends.map(friend => (
                 <FollowingItem
                     key={friend?.id}
