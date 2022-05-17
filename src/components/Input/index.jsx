1873db6473d45f5158fd5e611737f69fba0ee082
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-05-16 22:41:41 -0300

    🚀 :rocket: [Add] Input - id attribute

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -4,6 +4,7 @@ import styles from "./style.module.css"
 const Input = props => {
     return (
       <input className={`${styles.input} ${styles[props.className] ?? ''}`}
+      id={props.name.toLowerCase()}
             type={props.type}
             name={props.name}
             onChange={props.handleInputChange}
