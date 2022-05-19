92989632a87f0a2cbaf6341838e2a08e819d956b
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-05-19 16:52:30 -0300

    🚀 :rocket: [Add] Social item role attr

--- src/components/MainAppBar/Account/Social/SocialItem/index.jsx
+++ src/components/MainAppBar/Account/Social/SocialItem/index.jsx
@@ -1,7 +1,6 @@
 import styles from "./style.module.css"
 
 const SocialItem = (props) => {
-    console.log('props.children', props.children)
     return (
         <div className={styles.socialItem} role="menuitem"
          >
