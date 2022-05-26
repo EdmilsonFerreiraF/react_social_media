1a5f03979697be472f31814c5c9049d4c28b361a
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-05-25 23:32:55 -0300

    🚀 :rocket: [Add] SocialItem - test

--- src/components/MainAppBar/Account/Social/SocialItem/index.jsx
+++ src/components/MainAppBar/Account/Social/SocialItem/index.jsx
@@ -2,7 +2,7 @@ import styles from "./style.module.css"
 
 const SocialItem = (props) => {
     return (
-        <div className={styles.socialItem} role="menuitem"
+        <div data-testid="socialItem" className={styles.socialItem} role="menuitem"
          >
             {props.children}
             <span className={styles.socialItemBadge}>
---
+++ src/components/MainAppBar/Account/Social/SocialItem/index.test.js
@@ -0,0 +1,53 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import * as React from "react"
+import '@testing-library/jest-dom'
+import { Person } from '@mui/icons-material';
+import { axe, toHaveNoViolations } from 'jest-axe';
+
+import {
+  render,
+  screen,
+} from "components/customRender";
+import SocialItem from '.';
+
+expect.extend(toHaveNoViolations)
+
+describe('SocialItem', () => {
+  test('Should show person icon on profile button', async () => {
+    render(
+      <SocialItem badge={1}>
+        <Person />
+      </SocialItem>
+    )
+
+    expect(screen.getByTestId(/PersonIcon/i)).toBeInTheDocument();
+  })
+
+  test('Should show badge on profile button', async () => {
+    render(
+      <SocialItem badge={1}>
+        <Person />
+      </SocialItem>
+    )
+
+    expect(screen.getByText("1")).toBeInTheDocument();
+  })
+
+  test('Should be an acessible component', async () => {
+    const { container } = render(
+      <div role="menu" id="primary-search-account-menu">
+        <SocialItem badge={1}>
+          <Person />
+        </SocialItem>
+      </div>
+
+    )
+
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  })
+})
