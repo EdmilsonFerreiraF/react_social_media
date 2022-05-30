69d11888e63707d7a26ed23a257b2e75c202a0e5
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-05-30 17:30:43 -0300

    🎨 :art: [Refactor] FriendList && to ? chaining

--- src/components/Sidebar/FriendList/index.jsx
+++ src/components/Sidebar/FriendList/index.jsx
@@ -1,29 +1,24 @@
-import FriendItem from "../FriendItem"
-
-import { Users } from "dummyData"
+import { useContext } from 'react'
 
 import styles from "./style.module.css"
 import { baseUrl } from "constants/baseUrl"
-
 import { AuthContext } from "context/AuthContext"
-
-import { useContext } from 'react'
-
 import { useRequestData } from "hooks/useRequestData"
+import FriendItem from "../FriendItem"
 
 const FriendList = () => {
     const { user } = useContext(AuthContext)
 
-    // const url = `${baseUrl}/user/${user.id}/friends`
     const friends = useRequestData(`${baseUrl}/user/${user?.id}/friends`, [])
 
     console.log('friends', friends)
-    
+
     return (
         <ul className={styles.friendList} data-testid="friendList">
             {friends.map(friend => <FriendItem
-                    key={friend?.id}
-                    friend={friend} />
+                key={friend?.id}
+                friend={friend}
+            />
             )}
         </ul>
     )
