11786803f1679a27f0afd1cf629080bdb0e71dfb
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-05-30 17:42:43 -0300

    🚀 :rocket: [Add] Mock axios calls

--- __mocks__/axios.js
+++ __mocks__/axios.js
@@ -1,12 +1,6 @@
-// import { todos } from "../makeTodos";
-// import { getStorage, getDownloadURL, ref } from "firebase/storage"
-
-// const storage = getStorage();
-
-const imageUrl = `/profile/2d57d3b6-7a00-457b-9d3b-7b642cbdaf49.jpeg`
-
 const user = {
     _id: "6198494ec6ece6cbe6cdae4e",
+    id: 0,
     username: "user_username33",
     email: "user_email@email.com",
     profilePicture: "2d57d3b6-7a00-457b-9d3b-7b642cbdaf49.jpeg",
@@ -18,6 +12,7 @@ const user = {
 
 const friendUser = {
     _id: "6198494ec6ece6cbe6cdae4e",
+    id: 1,
     username: "user_username2",
     email: "user_email@email.com",
     profilePicture: "2d57d3b6-7a00-457b-9d3b-7b642cbdaf49.jpeg",
@@ -27,18 +22,105 @@ const friendUser = {
     followings: []
 }
 
-const get = jest.fn().mockImplementation((url) => {
+const friends = [
+    {
+        _id: "6198494ec6ece6cbe6cdae4e",
+        id: 0,
+        username: "user_username32",
+        email: "user_email@email.com",
+        profilePicture: "2d57d3b6-7a00-457b-9d3b-7b642cbdaf49.jpeg",
+        coverPicture: "",
+        isAdmin: false,
+        followers: [],
+        followings: []
+    },
+    {
+        _id: "6198494ec6ece6cbe6cdae4f",
+        id: 1,
+        username: "user_username2",
+        email: "user_email@email.com",
+        profilePicture: "2d57d3b6-7a00-457b-9d3b-7b642cbdaf49.jpeg",
+        coverPicture: "",
+        isAdmin: false,
+        followers: [],
+        followings: []
+    },
+    {
+        _id: "6198494ec6ece6cbe6cdae4g",
+        id: 2,
+        username: "user_username",
+        email: "user_email@email.com",
+        profilePicture: "2d57d3b6-7a00-457b-9d3b-7b642cbdaf49.jpeg",
+        coverPicture: "",
+        isAdmin: false,
+        followers: [],
+        followings: []
+    },
+]
 
-    if (url.includes(`user/user_username33`)) {
-        return Promise.resolve({ data: user });
-    } else if (url.includes(`user`)) {
-        return Promise.resolve({ data: friendUser });
-    } else if (url.includes("/post/profile/" ||
-        "/post/timeline/")) {
-        return Promise.resolve({ data: friendUser });
+
+const userPosts = [
+    {
+        id: 0,
+        userId: "bdb9322d-5868-4091-a305-d8cd9e4f62f4",
+        description: "user  -post description",
+        image: "0bf9c99f-a4cf-47eb-a856-7e79208d56b1.jpeg",
+        likes: [],
+    },
+    {
+        id: 1,
+        userId: "bdb9322d-5868-4091-a305-d8cd9e4f62f4",
+        description: "user  -post description",
+        image: "0bf9c99f-a4cf-47eb-a856-7e79208d56b1.jpeg",
+        likes: [],
+    },
+]
+
+const friendPosts = [
+    {
+        id: 0,
+        userId: "3a32071d-00f5-480e-bc93-b321299109ee",
+        description: "other user  -post description",
+        image: "0bf9c99f-a4cf-47eb-a856-7e79208d56b1.jpeg",
+        likes: [],
+    },
+    {
+        id: 1,
+        userId: "3a32071d-00f5-480e-bc93-b321299109ee",
+        description: "other user  -post description",
+        image: "0bf9c99f-a4cf-47eb-a856-7e79208d56b1.jpeg",
+        likes: [],
+    },
+]
+
+const post = jest.fn().mockImplementation((url) => {
+    if (url.includes(`/user/signup`)) {
+        return Promise.resolve();
     }
+})
     
-    throw new Error(`UNMATCHED URL: ${url}`);
+const get = jest.fn().mockImplementation((url, _) => {
+    console.log('jest mock url', url)
+    if (url.includes(`user/user_username2`)) {
+        return Promise.resolve({ data: friendUser });
+    } else if (url.includes('friends')) {
+        return Promise.resolve({ data: friends });
+    } else if (url.includes('user')) {
+        return Promise.resolve({ data: user });
+    } else if (url.includes("/post/timeline/")) {
+        return Promise.resolve({ data: userPosts });
+    } else if (url.includes("/post/profile/")) {
+        return Promise.resolve({ data: friendPosts });
+    } else {
+        return Promise.resolve({ data: user });
+    }
 })
 
-export default get
+const axios = {
+    __esModule: true,
+    get,
+    post,
+    default: () => new Promise(() => { }),
+}
+
+export default axios;
