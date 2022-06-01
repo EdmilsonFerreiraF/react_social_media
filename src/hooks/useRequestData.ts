299fd9789368d6961a2e3936eb2db44a75ff2ba2
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-06-01 12:58:26 -0300

    🚀 :rocket: [Add] Hooks - useRequestData types

---
+++ src/hooks/useRequestData.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { useErrorHandler } from 'react-error-boundary'
+
+export function useRequestData(url: string, initialState: any) {
+  const [data, setData] = useState(initialState)
+  const handleError = useErrorHandler()
+
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+
+    if (url) {
+      axios.get(url, {
+        headers: {
+          Authorization: token as string
+        }
+      }).then((response) => {
+        setData(response.data);
+      }).catch((error) => {
+        if (error.message.includes("401")) {
+          localStorage.removeItem("token")
+        }
+        
+        handleError(error)
+      })
+
+      return function cleanup() {
+        setData(initialState)
+      }
+    }
+  }, [url])
+
+  return data
+}
