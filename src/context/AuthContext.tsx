f7c8fe3293fd4871b60772f6ad0b4d9e160aa112
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-06-01 11:15:21 -0300

    🚀 :rocket: [Add] Context - AuthContext types

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,57 @@
+import React, { createContext, ReducerWithoutAction, useReducer } from "react"
+
+import AuthReducer from "./AuthReducer"
+
+interface User {
+    id: string,
+    username: string,
+    email: string,
+    profilePicture: string,
+    coverPicture: string,
+    isAdmin: boolean,
+    followers: [],
+    followings: []
+}
+
+interface AuthContextInterface {
+    user: User,
+    isFetching: boolean,
+    error: boolean,
+    dispatch: React.DispatchWithoutAction
+}
+
+const INITIAL_STATE = {
+    user: {
+        id: "",
+        username: "",
+        email: "",
+        profilePicture: "",
+        coverPicture: "",
+        isAdmin: false,
+        followers: [],
+        followings: []
+    },
+    isFetching: false,
+    error: false
+}
+
+export const AuthContext = createContext<AuthContextInterface | {}>({})
+
+export const AuthContextProvider = ({ children }: { children: JSX.Element }): JSX.Element => {
+    const [state, dispatch] = useReducer(AuthReducer as ReducerWithoutAction<any>, INITIAL_STATE)
+
+    const authContext: AuthContextInterface = {
+        user: state.user,
+        isFetching: state.isFetching,
+        error: state.error,
+        dispatch
+    };
+
+    return (
+        <AuthContext.Provider
+            value={authContext}
+        >
+        {children}
+        </AuthContext.Provider>
+    )
+}
