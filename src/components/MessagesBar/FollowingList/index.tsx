d28be69462b7be30286f3ae0e9c892864fbeadc9
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-06-02 12:33:44 -0300

    🚀 :rocket: [Add] FollowingList - types

--- src/components/MessagesBar/FollowingList/index.tsx
+++ src/components/MessagesBar/FollowingList/index.tsx
@@ -1,9 +1,12 @@
+import React from "react"
 import FollowingItem from "../FollowingItem"
 import styles from "./style.module.css"
 
+{/* @ts-ignore */}
 const FollowingList = ({ friends }) => {
     return (
         <div className={styles.followingList}>
+            {/* @ts-ignore */}
             {friends.map(friend => (
                 <FollowingItem
                     key={friend?.id}
