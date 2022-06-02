48f791b0f47a4f54bfb75f4517ea0ef5db45aa31
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-06-01 21:25:22 -0300

    🚀 :rocket: [Add] Profile - types

---
+++ src/components/MainAppBar/Account/Profile/index.tsx
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router"
+import React, { useContext } from "react";
+
+import { AuthContext, AuthContextInterface } from "context/AuthContext"
+import { useRequestImage } from "hooks/useRequestImage";
+
+import styles from "./style.module.css"
+
+const Profile = () => {
+    const { user } = useContext(AuthContext) as AuthContextInterface
+    
+    let navigate = useNavigate();
+
+    const goToUserProfile = () => {
+        navigate(`/profile/${user?.username}`)
+    }
+
+    const profilePicture = useRequestImage("profile", user?.profilePicture)
+
+    return (
+        <img className={styles.profileImg}
+            src={profilePicture}
+            alt="My profile"
+            onClick={goToUserProfile}/>
+    )
+}
+
+export default Profile
