6d584927fa6b148fb1a955ebb7f0459603258792
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-06-02 12:22:47 -0300

    🚀 :rocket: [Add] SearchBar - types

--- src/components/MainAppBar/SearchBar/index.tsx
+++ src/components/MainAppBar/SearchBar/index.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import { Search } from "@mui/icons-material"
 
 import styles from "./style.module.css"
---
+++ src/components/MainAppBar/SearchBar/style.module.css.d.ts
@@ -0,0 +1,4 @@
+export const searchBarContainer: string
+export const searchBar: string
+export const searchBarIcon: string
+export const searchBarInput: string
