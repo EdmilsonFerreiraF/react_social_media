927ebc864df115b53d58056802dc58719757f722
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-06-02 12:44:56 -0300

    🚀 :rocket: [Add] FriendList - types

--- src/components/MessagesBar/FriendList/index.tsx
+++ src/components/MessagesBar/FriendList/index.tsx
@@ -1,7 +1,9 @@
-import FriendItem from "../FriendItem"
+import React from "react"
+
+import FriendItem, { Friend } from "../FriendItem"
 import styles from "./style.module.css"
 
-const FriendList = ({ friends }) => {
+const FriendList = ({ friends }:  { friends: Friend[] }) => {
     return (
         <ul className={styles.friendList}>
             {friends.map(friend => (
