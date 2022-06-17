3d78c083d1cfaa0eb50f049739e48c389f45d010
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-06-17 20:58:03 -0300

    🎨 :art: [Refactor] Put SocialItems into array

--- src/components/MainAppBar/Account/Social/index.tsx
+++ src/components/MainAppBar/Account/Social/index.tsx
@@ -2,23 +2,25 @@ import React from 'react'
 import { Person, Chat, Notifications } from "@mui/icons-material"
 
 import SocialItem from "./SocialItem"
-
 import styles from "./style.module.css"
 
 const Social = () => {
+    const socialItems = [
+        <Person />,
+        <Chat />,
+        <Notifications />
+    ]
+
     return (
         <div data-testid="social"
             role="menu"
-            className={styles.social}>
-            <SocialItem badge={1}>
-                <Person />
-            </SocialItem>
-            <SocialItem badge={2}>
-                <Chat />
-            </SocialItem>
-            <SocialItem badge={3}>
-                <Notifications />
-            </SocialItem>
+            className={styles.social}
+        >
+            {socialItems.map((icon, idx) => (
+                <SocialItem badge={idx + 1}>
+                    {icon}
+                </SocialItem>
+            ))}
         </div>
     )
 }
