180b3dcdd42490ee8f92312650583752d74de81f
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-06-17 22:10:11 -0300

    🎨 :art: [Refactor] Encapsulate Content

---
+++ src/components/Post/Content/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import styles from "./style.module.css"
+
+const Content = (props: any) => {
+    return (
+        <div className={styles.content}>
+            <span data-testid="post content text"
+                className={"styles.contentText"}>
+                {!props.readMore ?
+                    <>
+                        <>{props.description.slice(0, 150)}
+                            <span className={styles.readMore}
+                                onClick={props.handleReadMore}>
+                                ... read more
+                            </span></>
+                    </>
+                    :
+                    <>
+                        {props.description}
+                        <span className={styles.readLess}
+                            onClick={props.handleReadMore}>
+                            read less
+                        </span>
+                    </>
+                }
+            </span>
+
+            <img data-testid="post content image"
+                className={styles.contentImg}
+                src={props.postPicture ?? props.noPostPicture}
+                alt="Post content" />
+        </div>
+    )
+}
+
+export default Content
---
+++ src/components/Post/Content/style.module.css.d.ts
@@ -0,0 +1,4 @@
+export const content: string
+export const readMore: string
+export const readLess: string
+export const contentImg: string
