17dbe5ebb5cbb2a6fc962b6d8caa663c15f3746b
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-06-17 21:57:41 -0300

    🚀 :rocket: [Add] TabletNavigation - test

---
+++ src/components/MainAppBar/Account/Navigation/TabletNavigation/index.test.tsx
@@ -0,0 +1,88 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import * as React from "react"
+import '@testing-library/jest-dom'
+import userEvent from "@testing-library/user-event";
+import { axe, toHaveNoViolations } from 'jest-axe';
+import { render as renderRTL } from '@testing-library/react';
+
+import {
+  render,
+  screen
+} from "components/CustomRender";
+import TabletNavigation from '.';
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import Profile from "../../Profile";
+
+expect.extend(toHaveNoViolations)
+
+describe('TabletNavigation', () => {
+  test('Should show navigation menu', async () => {
+    render(
+      <TabletNavigation />
+    )
+
+    expect(screen.getByTestId(/navigation menu/i)).toBeInTheDocument();
+  })
+
+  test('Should go to homepage page when it is clicked', async () => {
+    renderRTL(
+      <MemoryRouter initialEntries={["/profile/*"]}>
+        <Routes>
+          <Route
+            path="/profile/*"
+            element={
+              <TabletNavigation />
+            } />
+          <Route
+            path="/"
+            element={
+              <Profile />
+            } />
+        </Routes>
+      </MemoryRouter>
+    )
+
+    userEvent.click(screen.getByTestId(/homepageLink/i));
+
+    expect(screen.getByAltText(/My profile/i)).toBeInTheDocument();
+  })
+
+  test('Should go to timeline page when it is clicked', async () => {
+    renderRTL(
+      <MemoryRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <TabletNavigation />
+            } />
+          <Route
+            path="/profile/*"
+            element={
+              <Profile />
+            }
+          />
+        </Routes>
+      </MemoryRouter>
+    )
+
+    userEvent.click(screen.getByTestId(/timelineLink/i));
+
+    expect(screen.getByAltText(/My profile/i)).toBeInTheDocument();
+  })
+
+  test('Should be an acessible component', async () => {
+    const { container } = render(
+      <div role="menu" id="primary-search-account-menu">
+        <TabletNavigation />
+      </div>
+    )
+
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  })
+})
--- src/components/MainAppBar/Account/Navigation/index.tsx
+++ src/components/MainAppBar/Account/Navigation/index.tsx
@@ -1,99 +1,22 @@
-import React from 'react'
-import { useContext } from "react"
-import { useNavigate } from "react-router-dom"
-import HomeIcon from '@mui/icons-material/Home'
-import ArticleIcon from '@mui/icons-material/Article'
-import MenuItem from '@mui/material/MenuItem'
-import IconButton from '@mui/material/IconButton'
+import React from "react"
 
-import { AuthContext, AuthContextInterface } from 'context/AuthContext'
-import { goToIndex, goToProfile } from "routes/coordinator"
-import NavigationItem from "./NavigationItem"
-import styles from "./style.module.css"
+import { handleMenuOpening } from '../MobileMenu'
+import MobileNavigation from "./MobileNavigation"
+import TabletNavigation from "./TabletNavigation"
 
 type IProps = {
-    handleProfileMenuOpen?: () => void,
+    handleProfileMenuOpen?: handleMenuOpening,
     isMobileMenuOpen?: boolean
 }
 
 const Navigation = (props: IProps) => {
-    const { user } = useContext(AuthContext) as AuthContextInterface
-    const navigation = useNavigate()
-
-    const handleHomepageClick = () => {
-        goToIndex(navigation)
-    }
-
-    const handleTimelineClick = () => {
-        goToProfile(navigation, user?.username)
-    }
-
-    const NavigationMenu = (
-        <nav
-            data-testid="navigation menu"
-            className={styles.navigation}
-        >
-            <NavigationItem
-                handleClick={handleHomepageClick}
-                dataTestId="homepageLink"
-                title="Homepage"
-            />
-            <NavigationItem
-                handleClick={handleTimelineClick}
-                dataTestId="timelineLink"
-                title="Timeline"
-            />
-        </nav>
-    )
-
-    const NavigationMobileMenu = (
-        <div data-testid="navigation mobile menu">
-            <MenuItem
-                data-testid="navigationMenuItem"
-                onClick={props.handleProfileMenuOpen}
-            >
-                <IconButton
-                    data-testid="navigationIconButton"
-                    size="medium"
-                    aria-label="account of current user"
-                    aria-controls="primary-search-account-menu"
-                    aria-haspopup="true"
-                    color="inherit"
-                >
-                    <HomeIcon />
-                </IconButton>
-                <NavigationItem
-                    dataTestId="mobileHomepageLink"
-                    title="Homepage"
-                />
-            </MenuItem>
-            <MenuItem
-                data-testid="navigationMenuItem"
-                onClick={props.handleProfileMenuOpen}
-
-            >
-                <IconButton
-                    data-testid="navigationIconButton"
-                    size="medium"
-                    aria-label="account of current user"
-                    aria-controls="primary-search-account-menu"
-                    aria-haspopup="true"
-                    color="inherit"
-                >
-                    <ArticleIcon />
-                </IconButton>
-                <NavigationItem
-                    dataTestId="mobileTimelineLink"
-                    title="Timeline"
-                />
-            </MenuItem>
-        </div>
-    )
-
     return (
         props.isMobileMenuOpen ?
-            NavigationMobileMenu :
-            NavigationMenu
+            <MobileNavigation handleProfileMenuOpen={
+                props.handleProfileMenuOpen as handleMenuOpening 
+            }
+            /> :
+            <TabletNavigation />
     )
 }
 
