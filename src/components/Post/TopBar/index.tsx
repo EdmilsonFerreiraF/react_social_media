cdc90e3c7d5303a4ea6c434b264b13d2b83d4dc4
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-06-17 22:11:01 -0300

    🎨 :art: [Refactor] Encapsulate TopBar

---
+++ src/components/Post/TopBar/index.tsx
@@ -0,0 +1,36 @@
+import { MoreVert } from "@mui/icons-material";
+import React from "react";
+import { Link } from "react-router-dom";
+import { format } from "timeago.js";
+
+import styles from "./style.module.css"
+
+const TopBar = (props: any) => {
+    return (
+        <div className={styles.topbar}>
+                    <div className={styles.contentImg}>
+                        <Link to={`profile/${props.username}`}>
+                            <img src={props.profilePicture
+                                ??
+                                props.noProfilePicture}
+                                className={styles.profileImg}
+                                alt="Post user profile" />
+                        </Link>
+                        <span data-testid="post username"
+                            className={styles.username}>
+                            {props.username}
+                        </span>
+                        <span data-testid="post date"
+                            className={styles.date}>
+                            {format(props.createdAt)}
+                        </span>
+                    </div>
+                    <div data-testid="post options"
+                        className={styles.options}>
+                        <MoreVert />
+                    </div>
+                </div>
+    )
+}
+
+export default TopBar
---
+++ src/components/Post/TopBar/style.module.css.d.ts
@@ -0,0 +1,6 @@
+export const topbar: string
+export const contentImg: string
+export const profileImg: string
+export const username: string
+export const date: string
+export const options: string
