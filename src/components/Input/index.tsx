d45acbf00b6842f66cc4a48ec605624cbff6cc7c
Edmilson Ferreira <edmilsonf.o3@gmail.com>
2022-07-22 20:53:15 -0300

    🔨 :hammer: [Fix] Swapped, incorrect props

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -16,18 +16,18 @@ type IProps = {
 const Input = (props: IProps): JSX.Element => {
   const className = props.className as keyof typeof styles;
   const nameCapitalized =
-    props.type ?? props.name.charAt(0).toUpperCase() + props.name.slice(1);
+    props.name.charAt(0).toUpperCase() + props.name.slice(1);
 
   const inputProps = {
     className: `${styles.input} ${styles[className]}`,
     id: props.name.toLowerCase(),
-    type: props.type ?? nameCapitalized,
+    type: props.type ?? props.name,
     name: props.name,
     onChange: props.handleInputChange,
     value: props.value,
     placeholder: props.placeholder ?? nameCapitalized,
     "aria-invalid": props.invalid,
-    "data-testid": "input",
+    "data-testid": `${props.name}-input`,
     accept: props.accept,
   };
 
