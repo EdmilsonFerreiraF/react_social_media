00c2bbf79e0db39b511413cfe6bb8d2c15e9186c
Edmilson Ferreira <edmilsonf.o3@gmail.com>
2022-07-23 03:18:19 -0300

    🚀 :rocket: [Add] Swipeable drawer

--- src/components/MessagesBar/index.tsx
+++ src/components/MessagesBar/index.tsx
@@ -5,6 +5,9 @@ import { AuthContext, AuthContextInterface, User } from "context/AuthContext";
 import { useRequestData } from "hooks/useRequestData";
 import HomeMessagesBar from "./HomeMessagesBar";
 import ProfileMessagesBar from "./ProfileMessagesBar";
+
+import { Container } from "@mui/material";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 import styles from "./style.module.css";
 
 const MessagesBar = ({ user: visitedUser }: { user?: User }) => {
@@ -17,15 +20,66 @@ const MessagesBar = ({ user: visitedUser }: { user?: User }) => {
     []
   );
 
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setIsOpen(open);
+    };
+
+  const mobile = (
+    <>
+      <SwipeableDrawer
+        anchor="right"
+        open={isOpen}
+        onClose={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}
+        SwipeAreaProps={{
+          sx: {
+            display: { xs: "flex", md: "none" },
+          },
+        }}
+      >
+        <div className={styles.messagesBar}>
+          {visitedUser ? (
+            <ProfileMessagesBar user={user} friends={friends} />
+          ) : (
+            <HomeMessagesBar friends={friends} />
+          )}
+        </div>
+      </SwipeableDrawer>
+    </>
+  );
+
+  const desktop = (
+    <Container
+      className={styles.messagesBar}
+      sx={{
+        display: { xs: "none", md: "flex" },
+      }}
+      disableGutters={true}
+    >
+      {visitedUser ? (
+        <ProfileMessagesBar user={user} friends={friends} />
+      ) : (
+        <HomeMessagesBar friends={friends} />
+      )}
+    </Container>
+  );
+
   return (
     <div className={styles.messagesBarContainer}>
-      <div className={styles.messagesBar}>
-        {visitedUser ? (
-          <ProfileMessagesBar user={user} friends={friends} />
-        ) : (
-          <HomeMessagesBar friends={friends} />
-        )}
-      </div>
+      {mobile}
+      {desktop}
     </div>
   );
 };
