f18b6d0da13a05964047cedc58078653281a99b6
Edmilson Ferreira <edmilsonf.o3@gmail.com>
2022-07-23 03:12:17 -0300

    🚀 :rocket: [Add] Swipeable drawer

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,16 +1,59 @@
 import React from "react";
 
+import { Container } from "@mui/material";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 import FriendList from "./FriendList";
 import NavList from "./NavList";
 import styles from "./style.module.css";
 
 const Sidebar = () => {
-  return (
-    <aside
-      data-testid="sidebar"
-      aria-label="sidebarnavigation"
-      className={styles.sidebar}
-      aria-labelledby="sidebarnavigation"
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setIsOpen(open);
+    };
+
+  const mobile = (
+    <>
+      <SwipeableDrawer
+        anchor="left"
+        open={isOpen}
+        onClose={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}
+        sx={{
+          display: { xm: "flex", md: "none" },
+        }}
+      >
+        <div className={styles.sidebarContainer}>
+          <NavList />
+
+          <button className={styles.sidebarButton}>Show more</button>
+          <hr
+            data-testid="showMoreDivision"
+            className={styles.sidebarDivision}
+          />
+          <FriendList />
+        </div>
+      </SwipeableDrawer>
+    </>
+  );
+
+  const desktop = (
+    <Container
+      sx={{
+        display: { xs: "none", md: "flex" },
+      }}
+      disableGutters={true}
     >
       <div className={styles.sidebarContainer}>
         <NavList />
@@ -19,6 +62,18 @@ const Sidebar = () => {
         <hr data-testid="showMoreDivision" className={styles.sidebarDivision} />
         <FriendList />
       </div>
+    </Container>
+  );
+
+  return (
+    <aside
+      data-testid="sidebar"
+      aria-label="sidebarnavigation"
+      className={styles.sidebar}
+      aria-labelledby="sidebarnavigation"
+    >
+      {mobile}
+      {desktop}
     </aside>
   );
 };
