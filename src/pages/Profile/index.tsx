7b2cb6c2ccee6a2ba13f6e053a87c385479507f3
Edmilson Ferreira <edmilsonf.o3@gmail.com>
2022-07-31 16:56:15 -0300

    🔨 :hammer: [Fix] Firebase config not set

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -17,6 +17,20 @@ import noProfileImg from "img/no_person.webp";
 import styles from "./style.module.css";
 import feedStyles from "../../components/Feed/style.module.css";
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
+};
+
+initializeApp(firebaseConfig);
+
 const Feed = lazy(() => import("components/Feed"));
 
 const Profile = () => {
